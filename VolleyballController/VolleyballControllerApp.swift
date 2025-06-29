38eaa6bb907897911e659809c4c091033143c4d3
Konstantin S <havendv@gmail.com>
2025-06-29 08:03:52 +0400

    Revert "Remove unused files and assets from VolleyballController project, including ContentView, AppDelegate, and test files, to streamline the codebase."
    This reverts commit 803cf4b99d37f3270bef5cd30464c9c1d40c94d2.

---
+++ VolleyballController/VolleyballControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyballControllerApp.swift
+//  VolleyballController
+//
+//  Created by Konstantin Stukov on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct VolleyballControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
